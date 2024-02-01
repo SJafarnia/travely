79cda5c089e7dbd38b9072337693f4155c1ed3b7
Sadeq Jafarnia <s.jafarnia95@gmail.com>
2024-02-01 13:07:51 +0330

    formLabel changed to formSegment

--- src/components/modules/logs/form/components/FormSegment.tsx
+++ src/components/modules/logs/form/components/FormSegment.tsx
@@ -1,4 +1,3 @@
-
 type SegmentProps = {
     text: string,
     icon: React.ReactNode,
--- src/components/modules/logs/form/steps/Step1.tsx
+++ src/components/modules/logs/form/steps/Step1.tsx
@@ -1,26 +1,31 @@
-import { useFormikContext } from 'formik';
 import FormField from '../components/FormField';
 import { BsPassport } from "react-icons/bs";
 import { TbWorldQuestion } from "react-icons/tb";
 import NextStepIcon from '../components/icons/NextStepIcon';
 import FormButton from '../components/FormButton';
+import FormSegment from '../components/FormSegment';
 
 function Step1({ values, isSubmitting, nextStep }: { values: any, isSubmitting: boolean, nextStep: () => void }) {
 
     return (
         <>
-            <p className='text-center my-3 text-creamWhite'>
-                - Tell us what should be the title of your journey
-                <span><BsPassport className='inline mx-2 w-6 h-6' /></span>
-            </p>
-            <FormField name={"title"} title='Title of your journey' />
+            <FormSegment
+                text="- Tell us what should be the title of your journey"
+                styles="text-center my-3 text-creamWhite"
+                icon={<BsPassport className='inline mx-2 w-6 h-6' />}
+                formField={
+                    <FormField name={"title"} title='Title of your journey' />
+                }
+            />
 
-
-            <p className='my-3 text-creamWhite'>
-                - Can you tell us where on earth you've been?
-                <span><TbWorldQuestion className='inline mx-2 w-6 h-6' /></span>
-            </p>
-            <FormField name={"location"} title='Location (Continent, Country)' />
+            <FormSegment
+                text="- Can you tell us where on earth you've been?"
+                styles="my-3 text-creamWhite"
+                icon={<TbWorldQuestion className='inline mx-2 w-6 h-6' />}
+                formField={
+                    <FormField name={"location"} title='Location (Continent, Country)' />
+                }
+            />
 
             {values.title && values.location &&
                 <div className={`transition-opacity my-8 animate-fadeOut`}>
