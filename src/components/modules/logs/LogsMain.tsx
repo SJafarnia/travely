6dfb1ce9e9425086b517922f36e5081c01f83b07
Sadeq Jafarnia <s.jafarnia95@gmail.com>
2024-03-01 23:36:53 +0330

    form map resposive

--- src/components/modules/logs/LogsMain.tsx
+++ src/components/modules/logs/LogsMain.tsx
@@ -2,7 +2,7 @@ import LogForm from './form/LogForm';
 
 function LogsMain() {
     return (
-        <div className='mx-auto my-16 flex w-3/5 flex-col rounded-md bg-Night/95 p-8 text-creamWhite shadow-2xl shadow-forestGreen/50'>
+        <div className='mx-auto my-16 flex w-4/5 flex-col rounded-md bg-Night/95 p-8 text-creamWhite shadow-2xl shadow-forestGreen/50'>
             <LogForm />
         </div>
     );
